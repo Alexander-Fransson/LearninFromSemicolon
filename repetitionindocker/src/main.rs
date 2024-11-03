7738b9950458db095aeeffc8cd04e87ad32d2d60
Alexander-Fransson <festusOp7@gmail.com>
2024-11-03 19:32:18 +0100

    [feat] running server in docker

--- repetitionindocker/src/main.rs
+++ repetitionindocker/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http::StatusCode;
+use axum::{Json,Router};
+use axum::routing::get;
+use serde_json::{json, Value};
+
+async fn health_handler() -> (StatusCode, Json<Value>) {
+    let response = json!({
+        "Message": "App is running"
+    });
+    (StatusCode::OK, Json(response))
+}
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new()
+    .route("/health", get(health_handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    .await
+    .unwrap();
+
+    println!("Listening...");
+
+    let server = axum::serve(listener, router)
+    .await;
+
+    if let Err(err) = server {
+        eprintln!("Error: {}", err);
+    }
 }

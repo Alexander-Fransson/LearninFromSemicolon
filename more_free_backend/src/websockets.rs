df1f277a4a533c3eeec5557eaf2a2d146d0930d6
Alexander-Fransson <festusOp7@gmail.com>
2024-11-12 20:00:38 +0100

    [feat] made functions for websockets

---
+++ more_free_backend/src/websockets.rs
@@ -0,0 +1,23 @@
+use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
+use axum::response::IntoResponse;
+
+pub async fn websocket_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(|socket| handle_socket(socket))
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(Ok(msg)) = socket.recv().await {
+        match msg {
+            Message::Text(text) => {
+                println!("Got message from client: {}", text);
+                let formatted_response = format!("You said: {}", text);
+                socket.send(Message::Text(formatted_response)).await.unwrap();         
+            },
+            Message::Close(_) => {
+                println!("Client disconnected");
+                break;
+            },
+            _ => {}
+        }
+    }
+}

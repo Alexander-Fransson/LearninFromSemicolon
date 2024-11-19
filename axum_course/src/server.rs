d5413d2c7f81661dbe346b2e7a6e280c694cdf17
Alexander-Fransson <festusOp7@gmail.com>
2024-11-19 19:57:13 +0100

    [feat] dividing routes

--- axum_course/src/server.rs
+++ axum_course/src/server.rs
@@ -1,4 +1,4 @@
-use axum::extract::Query;
+use axum::extract::{Query, Path};
 use axum::response::Html;
 use axum::Router;
 use tokio::net::TcpListener;
@@ -7,9 +7,7 @@ use crate::models::HelloParams;
 use axum::response::IntoResponse;
 
 pub async fn server() {
-    let router = Router::new()
-    .route("/", get(hello_handler))
-    .route("/hello", get(hello_with_param_handler));
+    let router = Router::new().merge(basic_routes());
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Listening on http://127.0.0.1:3000");
@@ -17,12 +15,23 @@ pub async fn server() {
     axum::serve(listener, router).await.unwrap();   
 }
 
-pub async fn hello_handler() -> Html<&'static str> {
+fn basic_routes() -> Router {
+    Router::new()
+    .route("/", get(hello_handler))
+    .route("/hello", get(hello_with_param_handler))
+    .route("/hello/:name", get(hello_with_path_param_handler))
+}
+
+async fn hello_handler() -> Html<&'static str> {
     Html("<h1>Hello, world!</h1>")
 }
 
-pub async fn hello_with_param_handler(params: Query<HelloParams>) -> impl IntoResponse {
-    let name = &params.name.as_deref().unwrap_or("World");
+async fn hello_with_param_handler(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    let name = params.name.as_deref().unwrap_or("World");
 
     Html(format!("<h1>Hello, {}!</h1>", name))
+}
+
+async fn hello_with_path_param_handler(Path(name): Path<String>) -> impl IntoResponse {
+    Html(format!("<h1>Hello, {name}!</h1>"))
 }
--- axum_course/src/server_test.rs
+++ axum_course/src/server_test.rs
@@ -58,4 +58,25 @@ mod tests {
 
         server_thread.abort();
     }
+
+    #[tokio::test]
+    async fn test_hello_with_path_param() {
+        let server_thread = tokio::spawn(async move {
+            server().await;
+        });
+
+        tokio::time::sleep(Duration::from_millis(100)).await;
+
+        let client = Client::new();
+        let res = client.get("http://127.0.0.1:3000/hello/schopenhauer")
+        .send()
+        .await
+        .unwrap(); 
+
+        assert_eq!(res.status(), 200);
+        
+        let html = res.text().await.unwrap();
+        assert!(html.contains("<h1>Hello, schopenhauer!</h1>"));
+
+        server_thread.abort();}
 }

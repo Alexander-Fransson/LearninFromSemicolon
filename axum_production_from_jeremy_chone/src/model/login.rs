eaed4b3a2ccd9d7b6642261e75c27ce78a13b7a0
Alexander-Fransson <festusOp7@gmail.com>
2024-12-02 18:53:13 +0100

    [feat] decided to go through the course again before production to understand the steps more

--- axum_production_from_jeremy_chone/src/model/login.rs
+++
@@ -1,7 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-pub struct LoginPayload {
-    pub username: String,
-    pub pwd: String,
-}
--- axum_production_from_jeremy_chone/src/model/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod params_basic;
-pub mod login;

9db2b491b04a25dfcd47913c40b1a371467301da
Alexander-Fransson <festusOp7@gmail.com>
2024-12-09 20:14:35 +0100

    [feat] starting on database connection

---
+++ rust-web-app-E01-START/sql/dev-initial/00-recreate-db.sql
@@ -0,0 +1,9 @@
+-- DEV ONLY - Brute force DROP DB (for local dev and unit tests)
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE
+usename = 'app_user' OR datname = 'app_db';
+DROP DATABASE IF EXISTS app_db;
+DROP USER IF EXISTS app_user;
+
+-- DEV ONLY - Dev only password (for local dev and unit tests)
+CREATE USER app_user WITH PASSWORD 'dev_only_pwd';
+CREATE DATABASE app_db OWNER app_user ENCODING 'UTF-8';

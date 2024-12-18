6a4cce7ad4946fb16314c573788f2590a2251eff
Alexander-Fransson <festusOp7@gmail.com>
2024-12-18 20:23:30 +0100

    [feat] realized I need to create a derive macro

--- rust-web-app-E01-START/src/model/user.rs
+++ rust-web-app-E01-START/src/model/user.rs
@@ -7,8 +7,50 @@ use sqlx::postgres::PgRow;
 use sqlx::FromRow;
 use uuid::Uuid;
 
-#[derive(Clone, Debug, Serialize, FromRow)]
+use super::base::HasFields;
+
+
+pub trait HasFiledsForUser {
+
+}
+
+
+
+#[derive(Clone, Debug, Serialize, FromRow, HasFiledsForUser)]
 pub struct User {
     pub id: i64,
     pub username: String,
 }
+
+
+#[derive(Deserialize)]
+pub struct UserForCreate {
+    pub username: String,
+    pub pwd_clear: String,
+}
+
+struct UserForInsert {
+    pub username: String,
+}
+
+#[derive(FromRow, Clone, Debug, HasFiledsForUser)]
+pub struct UserForLogin {
+    pub id: i64,
+    pub username: String,
+    pub password: Option<String>,
+    pub pwd_salt: Uuid,
+    pub pwd_token_salt: Uuid,
+}
+
+#[derive(HasFiledsForUser)]
+pub struct UserForAuth {
+    pub id: i64,
+    pub username: String,    
+    pub pwd_salt: Uuid,
+}
+
+pub trait UserBy: for <'r> FromRow<'r, PgRow> + Unpin + Send + HasFiledsForUser {}
+
+impl UserBy for User {}
+impl UserBy for UserForLogin {} 
+impl UserBy for UserForAuth {}
